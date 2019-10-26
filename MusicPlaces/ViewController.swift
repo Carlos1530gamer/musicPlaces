00fb1276efd353db4663c39253b22274b7e98527
Carlos Daniel Hernandez Chauteco <carlos@bambu-mobile.com>
2019-10-25 20:36:37 -0500

    Initial Commit

---
+++ MusicPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlaces
+//
+//  Created by Carlos Bambu on 25/10/19.
+//  Copyright © 2019 Manzip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
